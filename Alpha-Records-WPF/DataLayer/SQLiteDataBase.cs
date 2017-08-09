745af3e79d1ff4c5e179c7502c41d9ca34dce840
ManchevCoding <manchevcoding@gmail.com>
2017-08-09 13:24:14 +0300

    Added db class

---
+++ Alpha-Records-WPF/DataLayer/SQLiteDataBase.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Records_WPF.DataLayer
+{
+    class SQLiteDataBase
+    {
+
+        private SQLiteConnection db = new SQLiteConnection("alpha.db");
+
+        public SQLiteConnection DB { get { return this.db; } }
+
+        public SQLiteDataBase()
+        {
+            db.CreateTable<ContactInfo>();
+        }
+    }
+}
