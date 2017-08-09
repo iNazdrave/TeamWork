30102a2b1b5d178a235248bef36f55fca1355840
ManchevCoding <manchevcoding@gmail.com>
2017-08-09 14:03:29 +0300

    added book class

---
+++ Alpha-Records-WPF/Models/Rank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Records_WPF.Models
+{
+    public class Rank
+    {
+        private string owner;
+        private int points;
+
+        public Rank(string owner, int points)
+        {
+            this.owner = owner;
+            this.points = points;
+        }
+
+        public string Owner { get { return this.owner; } }
+        public int Points { get { return this.points; } }
+    }
+}
