9b77e680fd0c2492f1eb5266c9c7e3c4caa556d9
Iliya Damyanov <i.s.damyanov@gmail.com>
2017-08-09 10:47:32 +0300

    Interface - IContact , IPresentation , class - ContantInfo , Presentation

---
+++ Alpha-Records-WPF/Models/ContactInfo.cs
@@ -0,0 +1,77 @@
+﻿using Alpha_Records_WPF.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Records_WPF.Model
+{
+    public class ContactInfo : IContactInfo
+    {
+        private string name;
+        private string username;
+        private string email;
+        private string phoneNumber;
+
+        public ContactInfo(string name, string username, string email, string phoneNumber)
+        {
+            this.name = name;
+            this.username = username;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string EMail
+        {
+            get
+            {
+                return this.email;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }        
+    }
+}
---
+++ Alpha-Records-WPF/Models/Presentation.cs
@@ -0,0 +1,47 @@
+﻿using Alpha_Records_WPF.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Records_WPF.Models
+{
+    public class Presentation : IPresentation
+    {
+        private DateTime date;
+        private string topic;
+
+        public Presentation(DateTime date, string topic)
+        {
+            this.date = date;
+            this.topic = topic;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.date;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Topic
+        {
+            get
+            {
+                return this.topic;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
